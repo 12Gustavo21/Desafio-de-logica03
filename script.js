09d726c10deb79bcece88a2c125ee81fcd5ecb88
12Gustavo21 <gustavo927318@gmail.com>
2022-06-15 16:54:21 -0300

    Desafio-de-logica03

---
+++ script.js
@@ -0,0 +1,21 @@
+//Questão 01
+for (let laco = 1; laco <= 10; laco++) {
+    console.log("Numero: " + laco);
+}
+
+//Questão 02
+let loop = 10;
+while (loop >= 1) {
+    console.log("Loop: " + loop);
+    loop--;
+}
+
+//Questão 03
+for (let numero = 1; numero <= 100; numero += 2) {
+    console.log("Numero impar: " + numero);
+}
+
+//Questão 04
+for (let numero = 2; numero <= 100; numero += 2) {
+    console.log("Numero par: " + numero);
+}
